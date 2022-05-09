0bcc4eddf1f11a57956a7673ed285a309a2fc904
Aidan <survivalking4@gmail.com>
2022-05-09 11:18:05 -0700

    first commit

---
+++ concepts/pointnorth/script.js
@@ -0,0 +1,22 @@
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
+function drawImage(evt) {
+    span.innerText = evt.alpha;
+    var angle = deg2rad(evt.alpha+180);
+    canvas.width = canvas.width; // clear canvas
+    ctx.beginPath();
+    
+    var x = Math.cos(angle)*10;
+    var y = Math.sin(angle)*10;
+
+    ctx.moveTo(50, 50);
+    ctx.lineTo(50+x, 50+y);
+    ctx.stroke();
+}
+
+window.ondeviceorientation = drawImage;
+
+function deg2rad(deg) {
+    return deg/180*Math.PI;
+}
